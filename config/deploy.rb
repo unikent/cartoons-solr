0f31e2fc30c892dd7aac28442d8a4c28f88da773
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-02-03 16:00:44 +0000

    Update caps

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,6 @@ set :git_enable_submodules, true
 set :deploy_via, :copy
 set :copy_compression, :zip
 
-after "deploy", "deploy:create_symlink"
-
 namespace :deploy do
   task :start do
     #run "cd #{app_path} && nohup java -jar start.jar > /opt/solr/shared/app.log 2>&1 &", :roles => :frontend
@@ -50,13 +48,14 @@ namespace :deploy do
 
   task :create_symlink do
   	run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
+
     indexes.each do |p| 
       run "mkdir -p #{current_path}/#{p}/data"
       run "ln -s #{shared_path}/indexes/#{p} #{current_path}/#{p}/data/index"
     end
 
-    run "ln -s #{latest_release}/solr.xml #{latest_release}/config/solr-master.xml", :roles => :processing
-    run "ln -s #{latest_release}/solr.xml #{latest_release}/config/solr-fe.xml", :roles => :frontend
+    run "ln -s #{latest_release}/config/solr-master.xml #{latest_release}/solr.xml", :roles => :processing
+    run "ln -s #{latest_release}/config/solr-fe.xml #{latest_release}/solr.xml", :roles => :frontend
   end
 
   task :finalize_update do
